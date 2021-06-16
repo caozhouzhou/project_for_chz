b3c11b5d5eda77e46be85f4a678eeb39bb465f40
caozhou_zch <caozhou_zch@163.com>
2021-06-16 23:40:00 +0800

    init project

---
+++ src/main/java/com/chz/project_for_chz/ProjectForChzApplication.java
@@ -0,0 +1,13 @@
+package com.chz.project_for_chz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForChzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectForChzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chz/project_for_chz/ProjectForChzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chz.project_for_chz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForChzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
